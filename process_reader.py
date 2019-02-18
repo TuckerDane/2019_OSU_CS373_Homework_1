106ffb70debde19a837ee8b955cbd03c422f0e66
TuckerDane <tuckerdwalker@gmail.com>
2019-02-17 18:02:39 -0800

    removes higherarchy

--- process_reader.py
+++ process_reader.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3.6
 
 import sys
+import psutil
 from texttable import Texttable
-
-sys.path.append('plugins/')
-
-from list_running_processes import list_running_processes
-from list_running_threads import list_running_threads
-from list_loaded_modules import list_loaded_modules
-from list_executable_pages import list_executable_pages
-from list_memory import list_memory
+from ctypes import string_at
+from sys import getsizeof
+from texttable import Texttable
 
 def main():
     print_menu()
@@ -62,7 +58,60 @@ def make_choice():
         except:
             print("invalid input\n")
             print_menu()
-        
-        
+
+def list_running_processes():
+    print("\nRunning Processes:")
+    running_processes = Texttable()
+    running_processes.add_rows([['PID', 'Process Name']])
+    for process in psutil.process_iter():
+        running_processes.add_row([process.pid, process.name()])
+    print(running_processes.draw())
+
+def list_running_threads(pid):
+    print("\nRunning Threads:")
+    process = psutil.Process(pid)
+    process_table = Texttable()
+    thread_table = Texttable()
+    process_table.add_rows([['PID', 'Process Name', 'Num Threads']])
+    process_table.add_row([process.pid, process.name(), process.num_threads()])
+    thread_table.add_rows([['Thread IDs']])
+    for thread in process.threads():
+        thread_table.add_row([thread.id])
+    print(process_table.draw())
+    print(thread_table.draw())
+
+def list_loaded_modules():
+    print("Enumerate all the loaded modules within a processes.")
+    loaded_modules = Texttable()
+    loaded_modules.add_rows([['Module Name']])
+    for module in sys.modules.keys():
+        loaded_modules.add_row([module])
+    print(loaded_modules.draw())
+
+def list_executable_pages(pid):
+    print("Is able to show all the executable pages within the processes.")
+    process = psutil.Process(pid)
+    process_table = Texttable()
+    maps_table = Texttable()
+    process_table.add_rows([['PID', 'Process Name']])
+    process_table.add_row([process.pid, process.name()])
+    maps_table.add_rows([['Memory Range', 'Offset', 'Path']])
+    maps_path = "/proc/"+str(process.pid)+"/maps"
+    fp = open(maps_path, 'r') 
+    for line in fp:
+        values = line.split()
+        if len(values) is 6:
+            for each in values:
+                maps_table.add_row([values[0], values[2], values[5]])
+        else:
+            for each in values:
+                maps_table.add_row([values[0], values[2], 'none'])
+    print(process_table.draw()) 
+    print(maps_table.draw())
+
+def list_memory(address):
+    print("read the memory.")
+    print(string_at(id(address), getsizeof(address)))
+
 if __name__ == "__main__":
     main()
