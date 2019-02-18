8973eda9e64fd59f6383ca343f15e958848b72e8
TuckerDane <tuckerdwalker@gmail.com>
2019-02-17 16:14:35 -0800

    adds loaded modules

--- plugins/list_loaded_modules.py
+++ plugins/list_loaded_modules.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3.6
 #source: https://pypi.org/project/psutil/
 
-import psutil
+import sys
 from texttable import Texttable
 
 def list_loaded_modules():
     print("Enumerate all the loaded modules within the processes.")
+    loaded_modules = Texttable()
+    loaded_modules.add_rows([['Module Name']])
+    for module in sys.modules.keys():
+        loaded_modules.add_row([module])
+    print(loaded_modules.draw())
